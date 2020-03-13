861f27aa5f46f1690c65e72370b4e5d97ef3475d
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-03-13 15:37:52 +0300

    feat: add script for portfolio

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const MENU = document.getElementById('menu');
+
 const VERT_PHONE = document.getElementById('vertical-phone');
 const HORIZ_PHONE = document.getElementById('horizontal-phone');
 const SLIDER_LEFT = document.getElementById('slider__left');
 const SLIDER_RIGHT = document.getElementById('slider__right');
 const SLIDER = document.getElementById('slider');
 
+const PORTFOLIO_TAG = document.getElementById('portfolio__tags');
+const PORTFOLIO_PROJECT = document.getElementById('portfolio__projects');
+
+
+// ------------------------------------------NAV
+
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(elem => elem.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+// ------------------------------------------SLIDER
 
 VERT_PHONE.addEventListener('click', (event) => {
     event.target.classList.toggle('bg-off');
@@ -18,29 +27,39 @@ HORIZ_PHONE.addEventListener('click', (event) => {
     event.target.classList.toggle('bg-off');
 });
 
-// ------------------------------------------SLIDER
+// let slideIndex = 1;
+// showSlide(slideIndex);
 
-let slideIndex = 1;
-showSlide(slideIndex);
+// SLIDER_LEFT.addEventListener('click', () => {
+//     showSlide(slideIndex = slideIndex - 1);
+// })
 
-SLIDER_LEFT.addEventListener('click', () => {
-    showSlide(slideIndex = slideIndex - 1);
+// SLIDER_RIGHT.addEventListener('click', () => {
+//     showSlide(slideIndex = slideIndex + 1);
+// })
+
+// function showSlide(n) {
+//     let slides = document.getElementById('slider_item');
+//     if (n>slides.length){
+//         slideIndex = 1;
+//     }
+//     if (n<1) {
+//         slideIndex = slides.length;
+//     }
+//     for (let i=0; i<slides.length; i++){
+//         slides[i].classList.add('hidden');
+//     }
+//     slides[slideIndex-1].classList.remove('hidden');
+// }
+
+// ------------------------------------------PORTFOLIO
+
+PORTFOLIO_TAG.addEventListener('click', (event) => {
+    PORTFOLIO_TAG.querySelectorAll('li').forEach(elem => elem.classList.remove('active'));
+    event.target.classList.add('active');
 })
 
-SLIDER_RIGHT.addEventListener('click', () => {
-    showSlide(slideIndex = slideIndex + 1);
+PORTFOLIO_PROJECT.addEventListener('click', (event) => {
+    PORTFOLIO_PROJECT.querySelectorAll('img.portfolio__projects-image').forEach(elem => elem.classList.remove('porfolio-border'));
+    event.target.classList.add('portfolio-border');
 })
-
-function showSlide(n) {
-    let slides = document.getElementById('slider_item');
-    if (n>slides.length){
-        slideIndex = 1;
-    }
-    if (n<1) {
-        slideIndex = slides.length;
-    }
-    for (let i=0; i<slides.length; i++){
-        slides[i].classList.add('hidden');
-    }
-    slides[slideIndex-1].classList.remove('hidden');
-}
